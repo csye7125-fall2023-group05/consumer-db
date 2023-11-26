735416d58fb73be1a302ee9a2cc9238732d62b79
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-11-26 13:52:18 -0500

    fix: init container db-migration error

--- sql/V0__create_table.sql
+++ sql/V0__create_table.sql
@@ -2,7 +2,7 @@ SET search_path TO app;
 
 CREATE TABLE httpchecks(
   id VARCHAR(255) PRIMARY KEY,
-  http_check_id VARCHAR(255) NOT NULL
+  http_check_id VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
   uri VARCHAR(255) NOT NULL,
   response_status_code INT NOT NULL,
